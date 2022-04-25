11e85b99e3b08e01744babd0f4a2ad0019dbbbe4
Dennizz <root@ipaddr.nl>
2022-04-25 21:25:55 +0200

    formatting

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 output "vpc" {
-    description = "The VPC created as an object with all attributes outputted. This is created from the aviatrix_vpc resource."
-    value       = var.use_existing_vpc ? null : aviatrix_vpc.mc_vpc[0]
+  description = "The VPC created as an object with all attributes outputted. This is created from the aviatrix_vpc resource."
+  value       = var.use_existing_vpc ? null : aviatrix_vpc.mc_vpc[0]
 }
 output "gateway" {
-    description = "The Aviatrix gateway created, as an object with all attributes outputted."
-    value       = aviatrix_gateway.gw
+  description = "The Aviatrix gateway created, as an object with all attributes outputted."
+  value       = aviatrix_gateway.gw
 }
